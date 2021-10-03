096e4d56a8eff01afb3b263af90dce55abc05796
KrishFX <88158135+KrishFX1@users.noreply.github.com>
2021-10-03 23:14:29 +0530

    Add files via upload

--- Chat/chat.js
+++ Chat/chat.js
@@ -1,20 +1,11 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyAsEEK1vGq3imxGGPopMFfcn5MDCq4UT8k",
-    authDomain: "storyweb-e902d.firebaseapp.com",
-    projectId: "storyweb-e902d",
-    storageBucket: "storyweb-e902d.appspot.com",
-    messagingSenderId: "124030460580",
-    appId: "1:124030460580:web:7e232959f35eec7c8b7c31",
-    measurementId: "G-VQQQMXEGD1"
+   <Your firebaseConfig>
 };
-
 
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 const firestore = firebase.firestore();
-
-
 
 document.addEventListener('DOMContentLoaded', function () {
     var blurEl = document.getElementById("elementWidth");
@@ -26,28 +17,42 @@ document.addEventListener('DOMContentLoaded', function () {
     })
     const fileSelector = document.getElementById('file-select');
     fileSelector.addEventListener('change', (event) => {
-        console.log("step 1")
         const fileList = event.target.files;
-        console.log(fileList)
+        const filelist = fileList
         if (fileList.length == 1) {
             var timestamp = Date.now()
-            console.log(fileList[0])
+            var reader = new FileReader();
+          
+            reader.onload = function(event) {
+                myFunction({
+                    name: currentUserName, message: event.target.result, RorL: 'right', type: "image", time: timestamp
+                })
+            };
+          
+            reader.readAsDataURL(fileList[0]);
+           
             var myHeaders = new Headers();
             myHeaders.append("Authorization", "Client-ID 2809817d5151af3");
             const formdata = new FormData()
-            formdata.append("image", fileList[0])
-            fetch("https://api.imgur.com/3/image", {
+            formdata.append("image", filelist[0])
+
+          fetch("https://api.imgur.com/3/image", {
                 method: "POST",
                 headers: myHeaders,
                 body: formdata,
                 redirect: 'follow'
-            }).then(data => data.json()).then(data => {
+            }).then(data => data.json()).then(async function (data) {
                 var img = data.data.link
-                console.log(img)
-                myFunction({
-                    name: currentUserName, message: '', RorL: 'left', type: "image", time: timestamp, img: img
+               
+               await firestore.collection("messages").add({
+                    message: img,
+                    userID: currentUserID,
+                    userName: currentUserName,
+                    type: 'image',
+                    timestamp: timestamp
                 })
-                db.collection("Messages").add({
+
+                db.collection("messages").add({
                     userName: currentUserName,
                     userID: currentUserID,
                     type: 'image',
@@ -57,18 +62,9 @@ document.addEventListener('DOMContentLoaded', function () {
                 userDB.collection("UserData").doc("UserID").update({
                     timestamp: timestamp
                 })
-                firestore.collection("Messages").add({
-                    message: img,
-                    userID: currentUserID,
-                    userName: currentUserName,
-                    type: 'image',
-                    timestamp: timestamp
-                })
             }).catch((err) => {
-                console.log(err)
                 alert("Image Upload Failed")
             })
-
         }
     });
 
@@ -78,7 +74,6 @@ document.addEventListener('DOMContentLoaded', function () {
     })
     document.addEventListener('keypress', function (event) {
 
-        console.log(event)
         if (event.key === 'Enter') {
             var timestamp = Date.now()
             var message = document.getElementById('elementWidth').value
@@ -89,7 +84,7 @@ document.addEventListener('DOMContentLoaded', function () {
                 myFunction({
                     name: currentUserName, time: timestamp, message: message, type: "chat", RorL: "right"
                 })
-                db.collection("Messages").add({
+                db.collection("messages").add({
                     userName: currentUserName,
                     userID: currentUserID,
                     type: 'chat',
@@ -99,7 +94,7 @@ document.addEventListener('DOMContentLoaded', function () {
                 userDB.collection("UserData").doc("UserID").update({
                     timestamp: timestamp
                 })
-                firestore.collection("Messages").add({
+                firestore.collection("messages").add({
                     message: message,
                     userID: currentUserID,
                     userName: currentUserName,
@@ -124,7 +119,7 @@ document.addEventListener('DOMContentLoaded', function () {
             })
             document.getElementById("Hi").setAttribute('href', `#${timestamp}`)
             document.getElementById("Hi").click()
-            db.collection("Messages").add({
+            db.collection("messages").add({
                 userID: currentUserID,
                 UserName: currentUserName,
                 type: 'chat',
@@ -134,7 +129,7 @@ document.addEventListener('DOMContentLoaded', function () {
             userDB.collection("UserData").doc("UserID").update({
                 timestamp: timestamp
             })
-            firestore.collection("Messages").add({
+            firestore.collection("messages").add({
                 message: message,
                 userName: currentUserName,
                 userID: currentUserID,
@@ -147,7 +142,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
 function myFunction({ name, time, message, RorL, type }) {
     var element
-    if (type == 'chat') {
+    if (type == "chat") {
         var ISTtime = new Date(time).toLocaleString(undefined, { timeZone: 'Asia/Kolkata' });
         var Time = ISTtime.split(', ')[1].split(':')
         var hour = Time[0]
@@ -156,19 +151,24 @@ function myFunction({ name, time, message, RorL, type }) {
 
         var Time = hour + ':' + minute + ' ' + AmPm
         if (RorL == 'left') {
-            element = `<li>  
-    <div class="left"> <section id="${time}"></section> <div style="font-size: 10px; color: #414141;">${name}</div> ${message} <div style="font-size: 8px;color:#4E4F51 ; font-style: italic; text-align: end;">${Time}</div> </div></li>`
+            element = `<li> <div class ="invisible-div"> 
+    <div class="left"> <section id="${time}"></section> <div style="font-size: 10px; color: #414141;">${name}</div> ${message} <div style="font-size: 8px;color:#4E4F51 ; font-style: italic; text-align: end;">${Time}</div> </div> </div></li>`
         } else {
             element = `<li>  
         <div class="right"> <section id="${time}"></section> ${message} <div style="font-size: 8px;color:#4E4F51 ; font-style: italic; text-align: end;">${Time}</div> </div></li>
    `
         }
-    } else if (type == 'join') {
+    } else if (type == "join") {
         element = `<li>  
         <div class="center">${name} joined the server <section id="${time}"></section>  </div></li>
     `
-    } else if (type == 'image') {
-        element = `<li> <div id='imageDiv'><img id="image"  class = 'left-Image' src='${message}'><section id="${time}"> </section> </div></li>`
+    } else if (type == "image") {
+        if (RorL == "left") {
+            element = `<li> <div class = "invisible-div"> <div id='leftImageDiv'><img id="image"  class = 'left-Image' src='${message}'><section id="${time}"> </section> </div> </div></li>`
+
+        } else {
+            element = `<li> <div id='rightImageDiv'><img id="image"  class = 'right-Image' src='${message}'><section id="${time}"> </section> </div></li>`
+        }
     }
 
     var mylist = document.getElementById('myList');
@@ -177,17 +177,14 @@ function myFunction({ name, time, message, RorL, type }) {
     document.getElementById("Hi").click()
 }
 
-var a = []
 let db = new Localbase('Database')
 let userDB = new Localbase("User")
 var currentUserID
 var currentUserName
 var lastTimestamp
 
+userDB.collection("UserData").doc("UserID").get().then(async function (userData) {
 
-
-userDB.collection("UserData").doc("UserID").get().then((userData) => {
-    console.log(userData)
     if (userData == null) {
         var timestamp = Date.now()
         var userName;
@@ -196,19 +193,21 @@ userDB.collection("UserData").doc("UserID").get().then((userData) => {
         } while (userName == null || userName.length < 4);
 
         myFunction({
-            type : 'join',
-            message : '',
-            time : timestamp , RorL : '',
-            name : userName
+            type: 'join',
+            message: '',
+            time: timestamp, RorL: '',
+            name: userName
         })
+        currentUserName = userName
 
-        firestore.collection("UserDB").doc("LastUserID").get().then((data) => {
+        
+        await firestore.collection("UserDB").doc("LastUserID").get().then((data) => {
             var lastID = Number(data.data().lastID)
             currentUserID = lastID + 1
-            console.log(currentUserID)
             firestore.collection("UserDB").doc("LastUserID").update({
                 lastID: currentUserID
             })
+
             userDB.collection("UserData").doc('UserID').set({
                 userID: currentUserID,
                 userName: userName,
@@ -217,15 +216,42 @@ userDB.collection("UserData").doc("UserID").get().then((userData) => {
             lastTimestamp = timestamp
         })
 
+        await firestore.collection("messages").add({
+            message: `joined`,
+            userName: userName,
+            userID: currentUserID,
+            type: 'join',
+            timestamp: timestamp
+        })
 
+        await firestore.collection("UserDB").add({
+            userID: currentUserID,
+            userName: userName,
+            timestamp: timestamp
+        })
     } else {
+
         userDB.collection("UserData").doc("UserID").get().then((userData) => {
             currentUserID = userData.userID
             currentUserName = userData.userName
+            var userNameForID
+            firestore.collection("UserDB").where("userID", '==', currentUserID).get().then((data) => {
+                if (data.empty == true) {
+                    userDB.delete()
+                    db.delete()
+                    window.location.reload();
+                } else {
+                    data.docs.forEach((doc) => {
+                        userNameForID = doc.userName
+                        if (userNameForID != currentUserName) {
+                        }
+                    })
+                }
+            })
         })
     }
 
-    db.collection('Messages').get().then((message) => {
+    db.collection('messages').get().then((message) => {
         if (message == []) { }
         else {
             var message = message
@@ -239,8 +265,7 @@ userDB.collection("UserData").doc("UserID").get().then((userData) => {
         }
         userDB.collection("UserData").doc('UserID').get().then((data) => {
             lastTimestamp = data.timestamp
-
-            firestore.collection("Messages").where('timestamp', '>', lastTimestamp).orderBy('timestamp').get().then((snapshot) => {
+            firestore.collection("messages").where('timestamp', '>', lastTimestamp).orderBy('timestamp').get().then((snapshot) => {
 
                 snapshot.docs.forEach(element => {
                     element = element.data()
@@ -248,10 +273,8 @@ userDB.collection("UserData").doc("UserID").get().then((userData) => {
                     if (element.userID == currentUserID) {
 
                     } else {
-                       myFunction({ name: element.userName, time: element.timestamp, message: element.message, type: element.type, RorL: 'left' })
-                        document.getElementById("Hi").setAttribute('href', `#${element.timestamp}`)
-                        document.getElementById("Hi").click()
-                        db.collection("Messages").add({
+                        myFunction({ name: element.userName, time: element.timestamp, message: element.message, type: element.type, RorL: 'left' })
+                        db.collection("messages").add({
                             userID: element.userID,
                             userName: element.userName,
                             type: element.type,
@@ -266,17 +289,15 @@ userDB.collection("UserData").doc("UserID").get().then((userData) => {
 
                 });
 
-               
-
-                var element = ` <li>
-                <div class="center"> New Messages <section id="${lastTimestamp}"></section>
-                </div>
+         var element = ` <li>
+               <div class = "invisible-div"> <div class="center"> New Messages <section id="${lastTimestamp}"></section>
+                </div></div>
             </li>`
 
                 var mylist = document.getElementById('myList');
                 mylist.insertAdjacentHTML('beforeend', element);
 
-                firestore.collection('Messages').where('timestamp', '>', lastTimestamp)
+                firestore.collection('messages').where('timestamp', '>', lastTimestamp)
                     .onSnapshot(querySnapshot => {
                         querySnapshot.docChanges().forEach(change => {
                             if (change.type === 'added') {
@@ -285,7 +306,7 @@ userDB.collection("UserData").doc("UserID").get().then((userData) => {
 
                                 } else {
                                     myFunction({ name: data.userName, time: data.timestamp, message: data.message, type: data.type, RorL: 'left' })
-                                     db.collection("Messages").add({
+                                    db.collection("messages").add({
                                         userName: data.userName,
                                         userID: data.userID,
                                         type: data.type,
@@ -305,6 +326,3 @@ userDB.collection("UserData").doc("UserID").get().then((userData) => {
         })
     })
 })
-
-
-
